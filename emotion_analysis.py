26f38c9e2546bd0341ff851ca175a224a8c066e8
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2022-08-05 04:20:26 +0300

    Add files via upload

---
+++ emotion_analysis.py
@@ -0,0 +1,30 @@
+# Calling the required libraries
+from textblob import TextBlob as tb
+
+
+# The function below, takes text and analyzes
+# it if it is negative or positive or neutral
+# then displays the right answer
+def sentiment_analysis(text):
+
+    if text.sentiment.polarity < 0:
+
+        print("Your thoughts are negative")
+        print(text.sentiment)
+
+    elif text.sentiment.polarity > 0:
+
+        print("Your thoughts are positive")
+        print(text.sentiment)
+
+    else:
+
+        print("Your thoughts are neutral")
+        print(text.sentiment)
+
+# input from user to check the program
+print("Please enter your text")
+text = tb(input())
+
+# calling the function
+sentiment_analysis(text)
